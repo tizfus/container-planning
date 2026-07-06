dd5a42453a760c6e22694ad4f3e2250011ec6cf6
tizfus <tizianof95@gmail.com>
2026-07-06 14:45:58 +0000

    improve base url path generation

--- tests/support/hooks.ts
+++ tests/support/hooks.ts
@@ -4,13 +4,22 @@ import CustomWorld from './world';
 setWorldConstructor(CustomWorld);
 setDefaultTimeout(60000);
 
-const PROJECT_FOLDER_NAME = 'container-planning'; 
-const PROJECT_PATH = __dirname.substring(0, __dirname.indexOf(PROJECT_FOLDER_NAME)) + PROJECT_FOLDER_NAME;
-const BASE_URL = `file://${PROJECT_PATH}/src/index.html`;
+
+function getBaseUrl(): string {
+    const reversedPath = __dirname.split('/').reverse();
+    
+    const projectFolderName = 'container-planning'; 
+    const projectPath = reversedPath.slice(reversedPath.indexOf(projectFolderName))
+        .reverse()
+        .join('/');
+
+    return `file://${projectPath}/src/index.html`;
+}
+
 
 Before(async function (this: CustomWorld) {
     await this.init();
-    await this.page.goto(BASE_URL);
+    await this.page.goto(getBaseUrl());
 });
 
 After(async function (this: CustomWorld) {
