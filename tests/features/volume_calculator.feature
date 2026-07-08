6115d3ee0e02432e8042842b61a8f334cfb48562
tizfus <tizianof95@gmail.com>
2026-07-08 21:59:57 +0000

    handle multiple products

--- tests/features/volume_calculator.feature
+++ tests/features/volume_calculator.feature
@@ -4,8 +4,11 @@ Feature: Volume Calculator
 
     Scenario Outline: Calculate volume for a product
         Given a product with length "<length>" width "<width>" height "<height>"
-        When I check the volume
+        When the volume is different than zero
+        And there is at least a product
         Then the total volume should be "<expected>"
+        And there should be "1" products
+        And last line is ready for the next product
 
         Examples:
             | length | width | height | expected |
@@ -24,3 +27,12 @@ Feature: Volume Calculator
         Then length should be "200"
         And width should be "200"
         And the height should be "200"
+
+    Scenario: Calculate volume for multiple products
+        Given a product with length "550" width "777" height "125"
+        And a product with length "200" width "300" height "400"
+        When the volume is different than zero
+        And there is at least a product
+        Then the total volume should be "77"
+        And there should be "2" products
+        And last line is ready for the next product
--- tests/steps/volume_calculator.ts
+++ tests/steps/volume_calculator.ts
@@ -8,9 +8,9 @@ Given('a product with length {string} width {string} height {string}', async fun
   width: string,
   height: string
 ) { 
-  await this.page!.getByTestId('length').first().fill(length);
-  await this.page!.getByTestId('width').first().fill(width);
-  await this.page!.getByTestId('height').first().fill(height);
+  await this.page!.getByTestId('length').last().fill(length);
+  await this.page!.getByTestId('width').last().fill(width);
+  await this.page!.getByTestId('height').last().fill(height);
 });
 
 When('I check the volume', async function (this: CustomWorld) {
@@ -47,4 +47,42 @@ Then('width should be {string}', async function (this: CustomWorld, expected: st
 Then('the height should be {string}', async function (this: CustomWorld, expected: string) {
   const value = await this.page!.getByTestId('height').first().inputValue();
   expect(value).toBe(expected);
+});
+
+When('the volume is different than zero', async function (this: CustomWorld) {
+  const totalVolumeText = await this.page!.getByTestId('totalVolume').textContent();
+  expect(parseFloat(totalVolumeText!)).toBeGreaterThan(0);
+});
+
+When('there is at least a product', async function (this: CustomWorld) {
+  const productsCount = await this.page!.getByTestId('product-size').count();
+  expect(productsCount).toBeGreaterThan(1);
+
+  var lengthValue = await this.page!.getByTestId('product-size').getByTestId('length').first().inputValue();
+  expect(lengthValue).not.toBe('');
+
+  var widthValue = await this.page!.getByTestId('product-size').getByTestId('width').first().inputValue();
+  expect(widthValue).not.toBe('');
+
+  var heightValue = await this.page!.getByTestId('product-size').getByTestId('height').first().inputValue();
+  expect(heightValue).not.toBe('');
+});
+
+Then('there should be {string} products', async function (this: CustomWorld, expected: string) {
+  const productRows = this.page!.getByTestId('product-size');
+  // Subtract 1 to exclude the empty row for the next product
+  const count = await productRows.count() - 1; 
+  expect(count).toBe(parseInt(expected));
+});
+
+Then('last line is ready for the next product', async function (this: CustomWorld) {
+  const productRows = this.page!.getByTestId('product-size');
+  const count = await productRows.count();
+  const lastRow = productRows.nth(count - 1);
+  const lengthValue = await lastRow.getByTestId('length').inputValue();
+  const widthValue = await lastRow.getByTestId('width').inputValue();
+  const heightValue = await lastRow.getByTestId('height').inputValue();
+  expect(lengthValue).toBe('');
+  expect(widthValue).toBe('');
+  expect(heightValue).toBe('');
 });
